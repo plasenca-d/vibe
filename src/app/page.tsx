67a7a4ab1fb0451a8b001ad07e25c8473e3b5045
Franzua Plasencia <franzua_plasencia@outlook.com>
2025-07-05 18:29:27 -0500

    feat: configure inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import Client from "./client";
-import { Suspense } from "react";
+"use client";
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.createAI.queryOptions({
-      text: "Franzua",
-    })
-  );
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => invoke.mutate({ text: "hello" })}>
+        Invoke BJ
+      </Button>
+    </div>
   );
 }
